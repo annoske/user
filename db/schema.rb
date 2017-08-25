e3e0d710ac7fb2606fc0eb98242990d552ecde9a
annryo <anno.ryoya-328@outlook.jp>
2017-08-25 12:38:47 +0900

    add item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825031618) do
+ActiveRecord::Schema.define(version: 20170825033711) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "us", force: :cascade do |t|
     t.string "name"
